be23845bd43cc372aa4fec5b33f983cd8d80c117
张宇亮 <zhangyuliang@zhangyuliangdeMacBook-Air.local>
2015-07-20 15:08:45 +0800

    2

---
+++ demo1/demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo1
+//
+//  Created by 张宇亮 on 15/7/20.
+//  Copyright (c) 2015年 张宇亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
